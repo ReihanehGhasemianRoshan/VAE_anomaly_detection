e06f7dc063599b3a5742d8de004999319e1f315f
ReihanehGhasemianRoshan <77486097+ReihanehGhasemianRoshan@users.noreply.github.com>
2023-02-20 20:00:09 +0330

    data

--- vae_anomaly_detection/dataset.py
+++ vae_anomaly_detection/dataset.py
@@ -1,6 +1,14 @@
 import torch
 from torch.utils.data import Dataset, TensorDataset
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.autograd import Variable
 
+train_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('data', train=True, download=True, transform=transforms.ToTensor()),
+    )
 
 def rand_dataset(num_rows=60_000, num_columns=100) -> Dataset:
     return TensorDataset(torch.rand(num_rows, num_columns))
